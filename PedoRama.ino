e4d6ae7e38805dd16a985f6b46713733cf55dac5
Ítalo Sousa <italusousa@gmail.com>
2018-11-06 22:41:12 -0300

    add steps count functions

--- PedoRama.ino
+++ PedoRama.ino
@@ -15,7 +15,7 @@ char ssid[] = "dexbie-ap";
 char pass[] = "8hq8xba9";
 bool isFirstConnect = true;
 
-// Accel
+// accel
 MPU6050 mpu;
 int SCL_PIN=D6;
 int SDA_PIN=D5;
@@ -28,11 +28,14 @@ int state = 6;
 
 void setup() {
   Serial.begin(9600);
+  Serial.println(">> Setup");
   programStatus();
   accelSetup();
+  calibrate();
   Blynk.begin(auth, ssid, pass);
   Blynk.virtualWrite(V0, "PedoRama");
   Alarm.timerRepeat(1, pedometerLoop);
+  Serial.println(">> End of setup");
 }
 
 void accelSetup() {
@@ -48,6 +51,7 @@ void programStatus() {
 }
 
 BLYNK_CONNECTED() {
+  Serial.println(">> Connected");
   if (isFirstConnect) {
     Blynk.syncAll();
     isFirstConnect = false;
@@ -76,8 +80,9 @@ void loop() {
   Blynk.run();
   Alarm.delay(100);
 }
+/*END OF SETUP*/
 
-/*FUNCTIONS*/
+/*BLYNK FUNCTIONS*/
 void changeMode(int pinData) {    
   Serial.print(pinData);
   if (pinData == 1) {
@@ -95,12 +100,102 @@ void changeView() {
   Blynk.virtualWrite(V0, statesName[state]);
 }
 
+/*END OF BLYNK FUNCTIONS*/
+
+/*PEDOMETER FUNCTIONS*/
+float threshhold=25.0;
+
+// valores da calibracao 
+float xval[50]={0};
+float yval[50]={0};
+float zval[50]={0};
+
+// medias
+float xavg; 
+float yavg;
+float zavg;
+int steps, flag = 0;
+
 void pedometerLoop() {
-  Vector normAccel = mpu.readNormalizeAccel();
-  Serial.print(" Xnorm = ");
-  Serial.print(normAccel.XAxis);
-  Serial.print(" Ynorm = ");
-  Serial.print(normAccel.YAxis);
-  Serial.print(" Znorm = ");
-  Serial.println(normAccel.ZAxis);
+  float totvect[50]={0}; // vetor de aceleracao
+  float totave[50]={0}; // vetor de medias
+
+  // aceleracoes no espaco de 50 interacoes
+  float xaccl[50]={0};
+  float yaccl[50]={0};
+  float zaccl[50]={0};
+
+  for (int i=0;i<50;i++) {
+    Vector normAccel = mpu.readNormalizeAccel();
+    xaccl[i]=float(normAccel.XAxis);
+    delay(1);
+    yaccl[i]=float(normAccel.YAxis);
+    delay(1);
+    zaccl[i]=float(normAccel.ZAxis);
+    delay(1);
+
+    // calculo de min e max de cada eixo
+    totvect[i] = sqrt(((xaccl[i]-xavg) * (xaccl[i]-xavg)) + ((yaccl[i] - yavg) * (yaccl[i] - yavg)) + ((zval[i] - zavg)* (zval[i] - zavg)));
+
+    // calculo do comprimento do passo
+    totave[i] = (totvect[i] + totvect[i-1]) / 2 ;
+
+    Serial.println(totave[i]);
+    delay(200);
+
+    //cal steps 
+    if (totave[i]>threshhold && flag==0) {
+      steps=steps+1;
+      flag=1;
+    } else if (totave[i] > threshhold && flag==1) {
+      //do nothing 
+    }
+
+    if (totave[i] <threshhold  && flag==1) {
+      flag=0;
+    }
+
+    Serial.println('\n');
+    Serial.print("steps=");
+    Serial.println(steps);
+  }
 }
+
+// Construir o filtro
+void calibrate() {
+  Serial.println(">> Calibrnado....");
+  float sum=0;
+  float sum1=0;
+  float sum2=0;
+  Vector normAccel = mpu.readNormalizeAccel();
+
+  // soma os valores do pino e seta no array 
+  for (int i=0;i<50;i++) {
+    xval[i] = float(normAccel.XAxis);
+    sum=xval[i]+sum;
+  }
+  xavg=sum/50.0;
+  Serial.println(xavg);
+  delay(100);
+
+
+  for (int j=0;j<50;j++) {
+    yval[j]=float(normAccel.YAxis);
+    sum1=yval[j]+sum1;
+  }
+  yavg=sum1/50.0;
+  Serial.println(yavg);
+  delay(100);
+
+  for (int k=0;k<50;k++) {
+    zval[k]=float(normAccel.ZAxis);
+    sum2=zval[k]+sum2;
+  }
+  zavg=sum2/50.0;
+  Serial.println(zavg);
+  delay(100);
+
+  Serial.println(">> Calibrado!");
+}
+
+/*END OF PEDOMETER FUNCTIONS*/
